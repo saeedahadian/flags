c83cad4b3a0dc43d08922df28becb55138fd110f
Saeed Ahadian <saeed.ahadian96@gmail.com>
2025-10-17 10:55:28 +0330

    fix: move the interface and types to `flags.go`.

--- flags.go
+++ flags.go
@@ -3,12 +3,62 @@ package flags
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Flag struct {
 	Key   string
 	Value FlagValue
 }
+
+type FlagValue interface {
+	String() string
+	Set(string) error
+}
+
+type intValue int
+
+func (i *intValue) String() string {
+	return strconv.Itoa(int(*i))
+}
+
+func (i *intValue) Set(value string) error {
+	val, err := strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+
+	*i = intValue(val)
+	return nil
+}
+
+type stringValue string
+
+func (s *stringValue) Set(value string) error {
+	*s = stringValue(value)
+	return nil
+}
+
+func (s *stringValue) String() string {
+	return string(*s)
+}
+
+type boolValue bool
+
+func (b *boolValue) Set(value string) error {
+	val, err := strconv.ParseBool(value)
+	if err != nil {
+		return err
+	}
+
+	*b = boolValue(val)
+	return nil
+}
+
+func (b *boolValue) String() string {
+	return strconv.FormatBool(bool(*b))
+}
+
 
 func NewIntFlag(key string, value string) (*Flag, error) {
 	var val intValue
--- parser.go
+++ parser.go
@@ -8,54 +8,6 @@ import (
 
 var flagKeyRegex = regexp.MustCompile(`-{1,2}(.+)`)
 
-type FlagValue interface {
-	String() string
-	Set(string) error
-}
-
-type intValue int
-
-func (i *intValue) String() string {
-	return strconv.Itoa(int(*i))
-}
-
-func (i *intValue) Set(value string) error {
-	val, err := strconv.Atoi(value)
-	if err != nil {
-		return err
-	}
-
-	*i = intValue(val)
-	return nil
-}
-
-type stringValue string
-
-func (s *stringValue) Set(value string) error {
-	*s = stringValue(value)
-	return nil
-}
-
-func (s *stringValue) String() string {
-	return string(*s)
-}
-
-type boolValue bool
-
-func (b *boolValue) Set(value string) error {
-	val, err := strconv.ParseBool(value)
-	if err != nil {
-		return err
-	}
-
-	*b = boolValue(val)
-	return nil
-}
-
-func (b *boolValue) String() string {
-	return strconv.FormatBool(bool(*b))
-}
-
 func ParseFlags(args []string) ([]*Flag, error) {
 	var flags []*Flag
 
